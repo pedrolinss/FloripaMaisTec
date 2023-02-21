99ed86bb04a2d40a690e22b72749165900a12662
“pedrolinss” <pedrolins1402@gmail.com>
2023-02-21 15:50:26 -0300

    adicionando exercicios da semana 2

---
+++ Modulo 01/Semana 02/ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Conta
+{
+	public class ContaBancaria
+	{
+		public int saque;
+		public int deposito;
+
+		public void Saldo()
+		{
+			Console.WriteLine($"Seu saldo atual é : {deposito - saque}");
+		}
+	}
+};
+
---
+++ Modulo 01/Semana 02/ContaBancaria/Program.cs
@@ -0,0 +1,16 @@
+﻿using Conta;
+
+ContaBancaria contaBancaria = new ContaBancaria();
+
+Console.WriteLine("Depositar :");
+contaBancaria.deposito = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Sacar :");
+contaBancaria.saque = int.Parse(Console.ReadLine());
+
+contaBancaria.Saldo();
+
+
+
+
+
