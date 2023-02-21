4797ebf2f57b3cdc37c2e2784cb60c70494014cf
“pedrolinss” <pedrolins1402@gmail.com>
2023-02-21 18:18:10 -0300

    Correção1 exercicio semana 2

---
+++ Modulo 01/Semana 02/ExercicioDaSemana/ContaBancaria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ExercicioDaSemana
+{
+    public class ContaBancaria
+    {
+        public int numero;
+        public string agencia;
+        public string nomeDoTitular;
+        public int saldo = 0;
+        public enum TipoDeConta
+        {
+            Corrente,
+            Poupança
+        };
+
+
+        public void Saldo(int deposito, int saque)
+        {
+            Console.WriteLine($"Seu saldo atual é : {deposito - saque}");
+        }
+    }
+};
+
---
+++ Modulo 01/Semana 02/ExercicioDaSemana/Program.cs
@@ -0,0 +1,12 @@
+﻿using ExercicioDaSemana;
+
+ContaBancaria contaBancaria = new ContaBancaria();
+
+Console.WriteLine("Digite o valor do deposito :");
+int deposito = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor do saque :");
+int saque = int.Parse(Console.ReadLine());
+
+
+contaBancaria.Saldo(deposito, saque);
